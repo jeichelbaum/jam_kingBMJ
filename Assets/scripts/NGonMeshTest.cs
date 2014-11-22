7ccd04a0490f0c94a2d5ae1dc6c137473c2a6b5c
jeichelbaum <trash.meil@gmx.de>
2014-11-22 15:32:54 +0100

    NGonMesh scale added

---
+++ Assets/scripts/NGonMeshTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NGonMeshTest : MonoBehaviour {
+
+    NGonMesh ngon;
+
+	// Use this for initialization
+	void Start () {
+        ngon = new NGonMesh();
+
+	}
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(new Vector3(0,0,0), 1);
+    }
+}
